da48cb373dcf0ff08a5c85c562d3ef9542284954
Tanner Dale <tanner@tannerdale.com>
2021-09-23 13:30:11 -0500

    Add tests, view stuff for discount links, next 3 holidays

--- spec/features/discounts/index_spec.rb
+++ spec/features/discounts/index_spec.rb
@@ -1,13 +1,21 @@
 require 'rails_helper'
 
+FakeHoliday = Struct.new(:name, :date)
+
 RSpec.describe 'discounts index' do
   describe 'index page' do
     let(:merchant) { create :merchant }
     let!(:discount1) { create :discount, { merchant_id: merchant.id } }
     let!(:discount2) { create :discount, { merchant_id: merchant.id } }
     let!(:discount3) { create :discount, { merchant_id: merchant.id } }
+    let!(:hol1) { FakeHoliday.new('Happy', Date.today) }
+    let!(:hol2) { FakeHoliday.new('Birthday', Date.today + 1) }
+    let!(:hol3) { FakeHoliday.new('You', Date.today + 2) }
+    let(:holidays) { [hol1, hol2, hol3] }
 
     before :each do
+      allow(HolidayService).to receive(:next_three).and_return(holidays)
+
       visit merchant_discounts_path(merchant)
     end
 
@@ -15,6 +23,23 @@ RSpec.describe 'discounts index' do
       [discount1, discount2, discount3].each do |discount|
         expect(page).to have_content(discount.quantity)
         expect(page).to have_content(discount.percentage.fdiv(100))
+      end
+    end
+
+    it 'has links to discount show pages' do
+      within "#discount-#{discount1.id}" do
+        click_on 'Discount Page'
+      end
+
+      expect(current_path).to eq(merchant_discount_path(merchant, discount1))
+    end
+
+    it 'has the next 3 discounts' do
+      within '#upcoming-holidays' do
+        holidays.each do |holiday|
+          expect(page).to have_content(holiday.name)
+          expect(page).to have_content(holiday.date.strftime('%b %d, %Y'))
+        end
       end
     end
   end
