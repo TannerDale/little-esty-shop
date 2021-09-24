b9950fde9ab92ec3517fcf6119e1472a403775e6
Tanner Dale <tanner@tannerdale.com>
2021-09-24 12:44:50 -0500

    Add scope for discounted items

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -3,20 +3,35 @@
 class InvoiceItem < ApplicationRecord
   self.primary_key = :id
 
+  belongs_to :invoice
   belongs_to :item
   has_one :merchant, through: :item
-  belongs_to :invoice
   has_many :discounts, through: :merchant
+  has_many :transactions, through: :invoice
 
   validates_presence_of :status
 
   enum status: %w[pending packaged shipped]
 
-  scope :not_shipped, lambda {
+  scope :not_shipped, -> {
     where.not(status: 2)
   }
 
-  scope :revenue, lambda {
+  scope :revenue, -> {
     sum('unit_price * quantity')
+  }
+
+  scope :discounted, -> {
+    quantities
+      .joins(:transactions, :discounts)
+      .group('discounts.id, invoice_items.item_id')
+      .select('discounts.*, invoice_items.item_id AS discount_item')
+      .merge(Transaction.success)
+      .merge(Discount.applicable)
+      .merge(Discount.ordered_percentage)
+  }
+
+  scope :quantities, -> {
+    select('SUM(invoice_items.quantity) AS amount')
   }
 end
