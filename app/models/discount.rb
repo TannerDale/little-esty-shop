43c00aefd4fe2ccc0f99f391d81aaa6cc6d6f690
Tanner Dale <tanner@tannerdale.com>
2021-09-24 12:43:35 -0500

    Add scopes for discounted items

--- app/models/discount.rb
+++ app/models/discount.rb
@@ -4,5 +4,15 @@ class Discount < ApplicationRecord
   has_many :invoice_items, through: :items
   has_many :invoices, through: :invoice_items
 
+  # validates :quantity, numerically: { greater_than: 0, only_integer: true }
+  # validates :percentage, numerically: { greater_than: 0, less_than_or_equal_to: 100, only_integer: true }
   validates_presence_of :quantity, :percentage
+
+  scope :applicable, -> {
+    having('SUM(invoice_items.quantity) >= discounts.quantity')
+  }
+
+  scope :ordered_percentage, -> {
+    order('discounts.percentage DESC')
+  }
 end
