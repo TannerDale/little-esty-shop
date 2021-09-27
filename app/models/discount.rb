a7892902b7f9de4bc7a5241994a8f0a94e9930fa
Tanner Dale <tanner@tannerdale.com>
2021-09-27 13:00:39 -0500

    Remove unneeded methods applicable, ordered_percentage

--- app/models/discount.rb
+++ app/models/discount.rb
@@ -6,12 +6,4 @@ class Discount < ApplicationRecord
 
   validates :quantity, numericality: { greater_than: 0, only_integer: true }
   validates :percentage, numericality: { greater_than: 0, less_than_or_equal_to: 100, only_integer: true }
-
-  scope :applicable, -> {
-    having('SUM(invoice_items.quantity) >= discounts.quantity')
-  }
-
-  scope :ordered_percentage, -> {
-    order('discounts.percentage DESC')
-  }
 end
