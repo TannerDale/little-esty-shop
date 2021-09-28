3e57ada29e0de00df1857d40619f70ffc6287739
Tanner Dale <tanner@tannerdale.com>
2021-09-28 10:50:35 -0500

    Make the github API a helper method instead of a before action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,7 @@
 
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  helper_method :current_user
-  before_action :github_api_data
+  helper_method :current_user, :github_api_data
 
   private
 
